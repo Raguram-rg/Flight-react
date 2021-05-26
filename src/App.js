bddcb9df4bae47448eac40562b1ea530dd4ef7f9
Raguram-rg <ragu.rg047@gmail.com>
2021-05-26 10:02:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [flights, setFlights] = useState([])
+
+  useEffect(() => {
+    const getFlights = async() => {
+      const responseFlights = await (await fetch('http://localhost:8080/flights')).json()
+      setFlights(responseFlights)
+    }     
+    getFlights();
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to Flight Checking</h1>
+      <table style={{border:'2px solid black',margin:'0 auto'}}>
+        <thead>
+          <tr style={{background:'rgb(255,0,0)',color:'white',border:'grey'}}>
+            <th>Flight Name</th>
+            <th>Source</th>
+            <th>Destination</th>
+            <th>Available Seats</th>
+            <th>Ticket Price</th>
+          </tr>
+        </thead>
+        {flights.map((flight,index) => {
+          return(
+            <tbody>
+              <tr key={index}>
+                <td>{flight.flightName}</td>
+                <td>{flight.source}</td>
+                <td>{flight.destination}</td>
+                <td>{flight.seatCount}</td>
+                <td>{flight.fare}</td>
+              </tr>
+            </tbody>
+          )
+        })}
+        </table>
     </div>
-  );
+  )
 }
 
 export default App;
